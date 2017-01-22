ec02d32818f096026ae66278d0f58e5e1d2beab4
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-01-22 17:21:36 +0000

    Add more functions
    Add `input_option_char`, `input_option_int` and `search_array` functions.

--- src/ruben9922/consoleutilities/inpututilities/__init__.py
+++ src/ruben9922/consoleutilities/inpututilities/__init__.py
@@ -23,3 +23,36 @@ def input_float(message):
             return number
         except ValueError as e:
             print("Numbers only! Decimals are allowed. ({0})".format(e))
+
+
+def input_option_char(valid_options, message="Enter option: "):
+    option = ""
+    input_valid = False
+    while not input_valid:
+        option = input(message).lower()
+        for valid_option in valid_options:
+            if option == valid_option:
+                input_valid = True
+                break
+        if not input_valid:
+            print("Invalid option!")
+    return option
+
+
+def input_option_int(titlelist, optioncount):
+    for i in range(0, len(titlelist)):
+        print("  {0}: {1}".format(i, titlelist[i]))
+    while True:
+        option = input_int("Enter option: ")
+        if 0 <= option < optioncount:
+            break
+        else:
+            print("Invalid option!")
+    return option
+
+
+def search_array(item, array):
+    for i in range(len(array)):
+        if item == array[i]:
+            return i
+    return -1
