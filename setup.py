f0885b3f4294a66abe290eee2536d3b1753b9a8e
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-06-08 17:53:56 +0100

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ruben-console-utilities",
-    version="0.1.1",
+    version="0.2.0",
     author="Ruben Dougall",
     # author_email="author@example.com",
     description="Simple utility functions for command-line applications.",
