1a9c0f702d51eeb11c1cad125ce2267d03ea7085
Ruben9922 <Ruben9922@users.noreply.github.com>
2020-06-08 19:03:32 +0100

    Edit setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,19 @@ setuptools.setup(
     name="ruben-console-utilities",
     version="0.2.0",
     author="Ruben Dougall",
-    # author_email="author@example.com",
+    author_email="info.ruebz999@gmail.com",
     description="Simple utility functions for command-line applications.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Ruben9922/python-console-utilities",
+    keywords="console command-line utilities",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Software Development :: Libraries :: Python Modules"
     ],
     python_requires='>=3.6',
 )
